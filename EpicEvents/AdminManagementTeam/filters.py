162fb4158c0427827d4d3b75ba92cb79e788a986
MDCarrasco <faultyquisby@protonmail.com>
2022-06-09 22:11:59 +0200

    [AdminManagementTeam] views and filters

---
+++ EpicEvents/AdminManagementTeam/filters.py
@@ -0,0 +1,32 @@
+from django_filters import rest_framework as filters
+from .models import Contract, Client, Event
+
+
+class ContractFilter(filters.FilterSet):
+    date_created = filters.CharFilter(field_name='date_created', lookup_expr='icontains')
+    amount = filters.NumberFilter(field_name='amount', lookup_expr='gte')
+    client_last_name = filters.CharFilter(field_name='client__last_name', lookup_expr='icontains')
+    client_email = filters.CharFilter(field_name='client__email', lookup_expr='iexact')
+
+    class Meta:
+        model = Contract
+        fields = ['date_created', 'amount', 'client']
+
+
+class ClientFilter(filters.FilterSet):
+    last_name = filters.CharFilter(field_name='last_name', lookup_expr='icontains')
+    email = filters.CharFilter(field_name='email', lookup_expr='iexact')
+
+    class Meta:
+        model = Client
+        fields = ['last_name', 'email', ]
+
+
+class EventFilter(filters.FilterSet):
+    event_date = filters.CharFilter(field_name='event_date', lookup_expr='icontains')
+    client_last_name = filters.CharFilter(field_name='client__last_name', lookup_expr='icontains')
+    client_email = filters.CharFilter(field_name='client__email', lookup_expr='iexact')
+
+    class Meta:
+        model = Event
+        fields = ['event_date', 'client']
